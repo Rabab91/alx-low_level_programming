6dbea062f54628e7bc6f65b5a0403b0c3ea6fe47
Rabab <Rababbenmassoud91@gmail.com>
2023-03-16 09:05:03 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c  program that prints exactly and that piece of art is useful"
- * - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * main - prints "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19, followed by a new line, to the standard error.
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+
 }

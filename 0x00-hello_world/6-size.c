54c0d29a19264a68c3cd21f083653c7499277a36
Rabab <Rababbenmassoud91@gmail.com>
2023-03-16 08:17:35 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program that print the size of various compter types using the printf function 
+ * main - prints the size using the printf function
  * Return: 0 (Success)
  */
 int main(void)
 {
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float f;
-printf("Size of a char: %d byte(s)", sizeof(a));
-printf("Size of an int: %d byte(s)", sizeof(b));
-printf("Size of a long int: %d byte(s)", sizeof(c));
-printf("Size of a long long int: %d  byte(s)", sizeof(d));
-printf("Size of a float: %d  bute(s)", sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }

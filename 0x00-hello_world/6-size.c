dde4f85ca697893f0ac3c34c4d0ab52388f594be
Rabab <Rababbenmassoud91@gmail.com>
2023-03-16 07:49:58 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
 printf("Size of a char: %lu bute(s)\n", (usingned long)sizeof(a));
 printf("Size of a int: %lu bute(s)\n", (usingned long)sizeof(b));
 printf("Size of a long int: %lu bute(s)\n", (usingned long)sizeof(c));

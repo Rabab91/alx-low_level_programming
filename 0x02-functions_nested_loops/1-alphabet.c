92d37974f3ca4aa7c497b7382917ad6fc65e19d3
Rabab <Rababbenmassoud91@gmail.com>
2023-03-21 10:36:50 -0700

    2task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets in lowercase
+ * uses the putchar function
+ * Return: nothing
+ */
+
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}

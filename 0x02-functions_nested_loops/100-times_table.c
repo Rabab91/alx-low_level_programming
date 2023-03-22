5934dc30fbaad2734ba63f4d3a2472be7c057dd9
Rabab <Rababbenmassoud91@gmail.com>
2023-03-21 17:34:16 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input, Starting with.
+ * print_times_table - Prints the times table of the input, Starting with 0.
  *
  * @n: The value of the times table to be printed.
  */
